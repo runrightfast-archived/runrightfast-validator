cf8f39e30d0ca3b5d80578d1fcd9ecbab04d63c3
Alfio Zappala <azappala@azaptree.com>
2013-10-16 06:30:37 -0400

    partially implemented  Property and tested

--- test/validator-domain.js
+++ test/validator-domain.js
@@ -19,13 +19,14 @@ var expect = require('chai').expect;
 var lodash = require('lodash');
 
 var validatorDomain = require('..').validatorDomain;
+var ObjectSchema = validatorDomain.ObjectSchema;
+var Type = validatorDomain.Type;
+var Property = validatorDomain.Property;
 
 describe('Validator Domain', function() {
 
-	describe('ObjectScehma', function() {
+	describe('ObjectSchema', function() {
 		it('must be constructed with options that have a valid namespace and version', function() {
-			var ObjectSchema = validatorDomain.ObjectSchema;
-
 			var options = {
 				namespace : 'ns://runrightfast.co/couchbase',
 				version : '1.0.0',
@@ -46,8 +47,6 @@ describe('Validator Domain', function() {
 		});
 
 		it('constructor options.namespace format must match pattern: ns://namespace', function(done) {
-			var ObjectSchema = validatorDomain.ObjectSchema;
-
 			var options = {
 				namespace : '//runrightfast.co/couchbase',
 				version : '1.0.0',
@@ -66,8 +65,6 @@ describe('Validator Domain', function() {
 		});
 
 		it('constructor options.version format must match pattern: x.x.x', function(done) {
-			var ObjectSchema = validatorDomain.ObjectSchema;
-
 			var options = {
 				namespace : 'ns://runrightfast.co/couchbase',
 				version : '1.0',
@@ -86,8 +83,6 @@ describe('Validator Domain', function() {
 		});
 
 		it('constructor options.description is required', function(done) {
-			var ObjectSchema = validatorDomain.ObjectSchema;
-
 			var options = {
 				namespace : 'ns://runrightfast.co/couchbase',
 				version : '1.0.0'
@@ -105,8 +100,6 @@ describe('Validator Domain', function() {
 		});
 
 		it('constructor options are required', function(done) {
-			var ObjectSchema = validatorDomain.ObjectSchema;
-
 			try {
 				new ObjectSchema();
 				done(new Error('expected validation error'));
@@ -115,8 +108,167 @@ describe('Validator Domain', function() {
 				expect(err._errors.length).to.equal(3);
 				done();
 			}
+		});
+
+		it('can list the names of Types that it contains', function() {
+			var options = {
+				namespace : 'ns://runrightfast.co/couchbase',
+				version : '1.0.0',
+				description : 'Couchbase config schema'
+			};
+
+			var schema = new ObjectSchema(options);
+			var typeNames = schema.getTypeNames();
+			expect(lodash.size(typeNames)).to.equal(0);
+
+			var type = new Type({
+				name : 'CouchbaseConnectionSettings',
+				description : 'Couchbase Connection Settings'
+			});
+
+			schema.addType(type);
+			expect(schema.getType(type.name)).to.exist;
+			expect(lodash.contains(schema.getTypeNames(), type.name)).to.equal(true);
+		});
+
+		it('can remove a Type by name', function() {
+			var options = {
+				namespace : 'ns://runrightfast.co/couchbase',
+				version : '1.0.0',
+				description : 'Couchbase config schema'
+			};
+
+			var schema = new ObjectSchema(options);
+
+			var type = new Type({
+				name : 'CouchbaseConnectionSettings',
+				description : 'Couchbase Connection Settings'
+			});
+
+			schema.addType(type);
+			expect(schema.getType(type.name)).to.exist;
+			expect(lodash.contains(schema.getTypeNames(), type.name)).to.equal(true);
+			var removedType = schema.removeType(type.name);
+			expect(removedType.name).to.equal(type.name);
+		});
+
+		it('a type with same name cannot be added twice', function(done) {
+			var options = {
+				namespace : 'ns://runrightfast.co/couchbase',
+				version : '1.0.0',
+				description : 'Couchbase config schema'
+			};
+
+			var schema = new ObjectSchema(options);
+
+			var type = new Type({
+				name : 'CouchbaseConnectionSettings',
+				description : 'Couchbase Connection Settings'
+			});
+
+			schema.addType(type);
+
+			try {
+				schema.addType(type);
+				done(new Error('expected an Error to be thrown because a type with the same name already exists'));
+			} catch (err) {
+				done();
+			}
+		});
+
+		it('#set - will replace existing types with the same name', function() {
+			var options = {
+				namespace : 'ns://runrightfast.co/couchbase',
+				version : '1.0.0',
+				description : 'Couchbase config schema'
+			};
+
+			var schema = new ObjectSchema(options);
+
+			var type = new Type({
+				name : 'CouchbaseConnectionSettings',
+				description : 'Couchbase Connection Settings'
+			});
+
+			schema.addType(type);
+			var type2 = new Type({
+				name : 'CouchbaseConnectionSettings',
+				description : 'Couchbase Connection Settings - 2'
+			});
+			schema.setType(type2);
+			expect(schema.getType(type.name).description).equal(type2.description);
 
 		});
+	});
+
+	describe('Type', function() {
+		it('must be constructed with a name', function(done) {
+			var options = {
+				name : 'CouchbaseConnectionSettings'
+			};
+
+			var type = new Type(options);
+			expect(type.name).to.equal(options.name);
+			expect(type.description).to.equal(options.name);
+			expect(type.allowOtherKeys).to.equal(false);
+
+			try {
+				type = new Type();
+				done(new Error('Expected Error to be thrown because Type requires name'));
+			} catch (err) {
+				done();
+			}
+		});
+
+		it('must be constructed with a name, description, and allowOtherKeys', function() {
+			var options = {
+				name : 'CouchbaseConnectionSettings',
+				description : 'Couchbase Connection Settings',
+				allowOtherKeys : true
+			};
+
+			var type = new Type(options);
+			expect(type.name).to.equal(options.name);
+			expect(type.description).to.equal(options.description);
+			expect(type.allowOtherKeys).to.equal(true);
+		});
+	});
+
+	describe('Property', function() {
+		it('must be constructed with a name and type', function(done) {
+			var options = {
+				name : 'port',
+				type : 'Number'
+			};
+
+			var property = new Property(options);
+			expect(property.name).to.equal(options.name);
+			expect(property.description).to.equal(options.name);
+			expect(property.type).to.equal(options.type);
+
+			try {
+				type = new Property();
+				done(new Property('Expected Error to be thrown because Property requires name and type'));
+			} catch (err) {
+				done();
+			}
+		});
+
+		it('must be constructed with a name and type that is supported', function(done) {
+			var options = {
+				name : 'port',
+				type : 'SDFSDFF'
+			};
+
+			try {
+				new Property(options);
+				done(new Property('Expected Error to be thrown because type is not supported'));
+			} catch (err) {
+				console.log(err);
+				done();
+			}
+		});
+
 	});
 
 });
